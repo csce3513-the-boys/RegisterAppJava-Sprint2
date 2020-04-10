d1ec6dd2ac1f3413802313a487a424762d9819f2
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2020-04-09 21:02:39 -0500

    Updated ViewNames.java

--- src/main/java/edu/uark/registerapp/controllers/enums/ViewNames.java
+++ src/main/java/edu/uark/registerapp/controllers/enums/ViewNames.java
@@ -5,8 +5,10 @@ public enum ViewNames {
 	MAIN_MENU("mainMenu"),
 	PRODUCT_DETAIL("productDetail"),
 	EMPLOYEE_DETAIL("employeeDetail"),
-	PRODUCT_LISTING("productListing");
-	
+	PRODUCT_LISTING("productListing"),
+	TRANSACTION("transaction"),
+	TRANSACTION_SUMMARY("transactionSummary");
+
 	public String getRoute() {
 		return this.route;
 	}

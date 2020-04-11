a6df6b424d182d96e22fc005d17b0207a1786b62
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2020-04-10 22:17:56 -0500

    created some getters for transaction.js

--- src/main/resources/static/scripts/transaction.js
+++ src/main/resources/static/scripts/transaction.js
@@ -1,1 +1,15 @@
+// Getters and setters
+function getSubmitActionElement() {
+	return document.getElementById("productSearch");
+}
 
+function getAddActionElement() {
+	return document.getElementById("addButton");
+}
+
+function getSearchElement() {
+	return document.getElementById("search");
+}
+
+//ADD price getters
+// End getters and setters

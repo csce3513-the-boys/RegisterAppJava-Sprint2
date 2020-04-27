73e78f07c697ca939cb553b61ec92ce2cbfd6775
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2020-04-27 12:43:05 -0500

    added new controllers

---
+++ src/main/java/edu/uark/registerapp/controllers/cashierSalesController.java
@@ -0,0 +1,37 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.enums.EmployeeClassification;
+
+@Controller
+@RequestMapping(value = "/cashierSales")
+public class TransactionRouteController extends BaseRouteController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView showTransaction(final HttpServletRequest request)
+	{
+			ModelAndView modelAndView = new ModelAndView(ViewNames.CASHIER_SALES.getViewName());
+
+			final Optional<ActiveUserEntity> activeUserEntity = this.getCurrentUser(request);
+			if(!activeUserEntity.isPresent())
+			{
+					return buildInvalidSessionResponse();
+			}
+			return modelAndView;
+	}
+}
